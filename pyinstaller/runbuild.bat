8abad697982b5cfb60e278742d15cd19004c3e77
jussi.rasinmaki <jussi.rasinmaki@simosol.fi>
2009-02-26 12:10:09 +0000

    build script updates

--- pyinstaller/runbuild.bat
+++ pyinstaller/runbuild.bat
@@ -1,4 +1,7 @@
-python c:/pyinstaller/Build.py yasso.spec
+cd ..
+python C:\Python25\Scripts\f2py.py -c --fcompiler=gnu95 -m y07 y07_subroutine.f90
+cd pyinstaller
+python c:\pyinstaller\Build.py yasso.spec
 cd ..
 copy demo_data.txt pyinstaller\dist
 copy yasso_param.dat pyinstaller\dist
--- pyinstaller/runbuild.sh
+++ pyinstaller/runbuild.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+cd ..
+f2py2.5 -c --fcompiler=gnu95 -m y07 y07_subroutine.py
+cd pyinstaller
 python ~/pyinstaller/Build.py yasso.spec
 cp ../demo_data.txt dist/
 cp ../yasso_param.dat dist/
