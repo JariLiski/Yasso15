334e52b1e0e29ac6da7bc7a6fd06aa5674dcb6d9
jussi.rasinmaki <jussi.rasinmaki@simosol.fi>
2009-03-18 16:04:29 +0000

    cleaned debug from yasso.spec

--- pyinstaller/runbuild.sh
+++ pyinstaller/runbuild.sh
@@ -4,6 +4,6 @@ f2py2.5 -m y07 y07_subroutine.py
 f2py2.5 -c --fcompiler=gnu95 -m y07 y07_subroutine.py
 cd pyinstaller
 python ~/pyinstaller/Build.py yasso.spec
-cp ../demo_data.txt dist/
+cp ../demo_input.txt dist/
 cp ../yasso_param.dat dist/
 dist/yasso
